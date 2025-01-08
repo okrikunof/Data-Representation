98c6c03d84e60222cecb734b8a82753e032c1f19
Oleg Krikunov <okrikunof@gmail.com>
2025-01-08 22:08:06 +0300

    add test for ListMap

---
+++ src/Tests/MapTest.java
@@ -0,0 +1,28 @@
+package Tests;
+
+import ListElement.*;
+
+import Maps.LinkedListMap.*;
+
+public class MapTest {
+    public static void Test() {
+        Map map = new Map();
+
+        ListElement pair1 = new ListElement("Paris", "France");
+        map.Assign(pair1);
+        map.Print();
+        ListElement pair2 = new ListElement("London", "England");
+        map.Assign(pair2);
+        map.Print();
+        ListElement pair3 = new ListElement("Paris", "Russia");
+        map.Assign(pair3);
+        map.Print();
+
+        ListElement pair4 = new ListElement("Tumba", "Umba");
+        map.Compute(pair4);
+
+        ListElement pair5 = new ListElement("London", "Zimbabwe");
+        System.out.println(map.Compute(pair5));
+        map.Print();
+    }
+}
