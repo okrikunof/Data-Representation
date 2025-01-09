bcc21dcb572cd3dd7f5486f59d3baee978aab91f
Oleg Krikunov <okrikunof@gmail.com>
2025-01-09 09:27:34 +0300

    add test for list

---
+++ src/Tests/ListTest.java
@@ -0,0 +1,49 @@
+package Tests;
+
+import ListElement.ListElement;
+
+import Lists.ArrayList.*;
+//import Lists.LinkedList.*;
+//import Lists.DoubleLinkedList.*;
+//import Lists.CursorList.*;
+
+public class ListTest {
+    public static void Test() {
+        List list = new List();
+
+        list.Insert(new ListElement("1", "A"), list.First());
+        list.Insert(new ListElement("2", "B"), list.End());
+        list.Insert(new ListElement("2", "B"), list.End());
+        list.Insert(new ListElement("2", "B"), list.End());
+        list.Insert(new ListElement("2", "B"), list.End());
+        list.Insert(new ListElement("3", "C"), list.First());
+        list.Insert(new ListElement("1", "A"), list.End());
+        list.Insert(new ListElement("2", "B"), list.End());
+        list.Insert(new ListElement("2", "B"), list.End());
+        list.Insert(new ListElement("2", "B"), list.End());
+
+        list.PrintList();
+
+        RemoveDuplicates(list);
+
+        System.out.println("\n");
+
+        list.PrintList();
+    }
+
+    public static void RemoveDuplicates(List list) {
+        // Удаление дубликатов
+        Position p = list.First();
+        while (!p.Equals(list.End())) {
+            Position q = list.Next(p);
+            while (!q.Equals(list.End())) {
+                if (list.Retrieve(p).Equals(list.Retrieve(q))) {
+                    list.Delete(q);
+                } else {
+                    q = list.Next(q);
+                }
+            }
+            p = list.Next(p);
+        }
+    }
+}
