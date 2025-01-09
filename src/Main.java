cebaae46d7f26d42c0d289de8f141b6b9c20a3ca
Oleg Krikunov <okrikunof@gmail.com>
2025-01-09 09:26:26 +0300

    move test for list in separate file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import ListElement.*;
-
 import Tests.*;
 
 import Lists.ArrayList.*;
@@ -7,47 +5,12 @@ import Lists.ArrayList.*;
 //import Lists.DoubleLinkedList.*;
 //import Lists.CursorList.*;
 
-
 public class Main {
     public static void main(String[] args) {
-//        List list = new List();
-//
-//        list.Insert(new ListElement("1", "A"), list.First());
-//        list.Insert(new ListElement("2", "B"), list.End());
-//        list.Insert(new ListElement("2", "B"), list.End());
-//        list.Insert(new ListElement("2", "B"), list.End());
-//        list.Insert(new ListElement("2", "B"), list.End());
-//        list.Insert(new ListElement("3", "C"), list.First());
-//        list.Insert(new ListElement("1", "A"), list.End());
-//        list.Insert(new ListElement("2", "B"), list.End());
-//        list.Insert(new ListElement("2", "B"), list.End());
-//        list.Insert(new ListElement("2", "B"), list.End());
-//
-//        list.PrintList();
-//
-//        RemoveDuplicates(list);
-//
-//        System.out.println("\n");
-//
-//        list.PrintList();
+        ListTest.Test();
 //        StackTest.Test();
 //        QueueTest.Test();
-        MapTest.Test();
-    }
-
-    public static void RemoveDuplicates(List list) {
-        // Удаление дубликатов
-        Position p = list.First();
-        while (!p.Equals(list.End())) {
-            Position q = list.Next(p);
-            while (!q.Equals(list.End())) {
-                if (list.Retrieve(p).Equals(list.Retrieve(q))) {
-                    list.Delete(q);
-                } else {
-                    q = list.Next(q);
-                }
-            }
-            p = list.Next(p);
-        }
+//        MapTest.Test();
+//        PriorityQueueTest.Test();
     }
 }
